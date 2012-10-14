cd1e65868a7ca0ef5d0e4b72ecae47e814875fbb
David Reignier <david.reignier@gmail.com>
2012-10-14 13:31:10 +0200

    Add the authentication library.

---
+++ www/metalizer/library/authentication/model/BaseUser.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ Metalizer, a MVC php Framework.
+ Copyright (C) 2012 David Reignier
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+/**
+ * Represent a minimal user. It just handle name, email and password.
+ * @author David Reignier
+ *
+ */
+class BaseUser extends Model {
+   
+   /**
+    * Set the email.
+    * @param $email string
+    *    A valid email.
+    */
+   public function setEmail($email) {
+      $this->model->email = $email;
+   }
+   
+   /**
+    * Set the password. The password will be encrypted using the crypt php function. The salt is auto generated.
+    * @param $password string
+    *    A clear password.
+    */
+   public function setPassword($password) {
+      $this->model->password = crypt($password);
+   }
+   
+   /**
+    * Set the login.
+    * @param $login string
+    *    A login.
+    */
+   public function setLogin($login) {
+      $this->model->login = $login;
+   }
+   
+   /**
+    * @return string
+    *    The email of the user.
+    */
+   public function getEmail() {
+      return $this->model->email;
+   }
+ 
+   /**
+    * @return login
+    *    The login of the user
+    */
+   public function getLogin() {
+      return $this->model->login;
+   }
+   
+   /**
+    * @param $password string
+    *    A clear password.
+    * @return bool
+    *    true if the given clear password is the password of this user. false otherwise.
+    */
+   public function challengePassword($password) {
+      return crypt($password, $this->model->password) == $this->model->password; 
+   }
+} 
---
+++ www/metalizer/library/authentication/util/AuthenticationUtil.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ Metalizer, a MVC php Framework.
+ Copyright (C) 2012 David Reignier
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+define('AUTHENTICATION_SESSION_NAME', "authentication.user");
+ 
+/**
+ * Provide easy access to authentication functions. AuthenticationUtil use BaseUser to represent users.
+ * If you want to extend the BaseUser behavior, just use a subclass.
+ * @author David Reignier
+ *
+ */
+class AuthenticationUtil extends Util {
+   
+   /**
+    * The current user
+    * @var BaseUser
+    */
+   private $currentUser = null;
+
+   /**
+    * We must clean the current user.
+    */
+   public function onSleep() {
+      $this->currentUser = null;
+   }
+
+   /**
+    * Do an authentication challenge. 
+    * @param $login string
+    *    A login or an email.
+    * @param $password string
+    *    A password
+    * @return mixed
+    *    if the login/password couple is correct, the corresponding BaseUser is returned. Otherwise, null is returned.
+    */
+   public function challenge($login, $password) {
+      $user = model('BaseUser')->findOneBy('login', $login);
+      if (!$user) {
+         $user = model('BaseUser')->findOneBy('email', $login);
+      }
+      
+      if (!$user) {
+         return null;
+      }
+      
+      return $user->challengePassword($password) ? $user : null;
+   }
+   
+   /**
+    * Login a user.
+    * @param $user BaseUser
+    *    A base user.
+    * @param 
+    */
+   public function login($user, $cookie = false) {
+      $this->currentUser = $user;
+      session()->set(AUTHENTICATION_SESSION_NAME, $user->getLogin());
+   }
+   
+   /**
+    * Logout the current user. Do nothing is their's no current user.
+    */
+   public function logout() {
+      session()->clean(AUTHENTICATION_SESSION_NAME);
+      $this->currentuser = null;
+   }
+   
+   /**
+    * Get the current user.
+    * @return mixed
+    *    The current BaseUser if the current user is authenticated. null otherwise.
+    */
+   public function getCurrentUser() {
+      if ($this->currentUser) {
+         return $this->currentUser;
+      }   
+      
+      $login = session()->get(AUTHENTICATION_SESSION_NAME);
+      
+      if (!$login) {
+         return null;
+      }
+      
+      $user = model('BaseUser')->findOneBy('login', $login);
+      
+      if (!$user) {
+         return null;
+      }
+      
+      $this->currentUser = $user;
+      return $user;
+   }
+
+}
+
+function getCurrentUser() {
+   return util('Authentication')->getCurrentUser();
+}
